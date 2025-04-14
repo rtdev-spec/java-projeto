939e03c87009cb171677dacbcb3342da6f75e559
rtdev-spec <rtzikadeveloper@gmail.com>
2025-04-14 10:54:37 -0300

    Commit-3

---
+++ java-nativec-loud/src/curso-java/renato/primeiro arquivo java/boletim.java
@@ -0,0 +1,12 @@
+public class boletim {
+    public static void main(String[] args) {
+
+    int media = 6;
+        if (media < 5)
+            System.out.println("reprovado");
+        else if (media == 5)
+            System.out.println("Prova final");
+       else
+            System.out.println("Aprovado");
+    }
+}
---
+++ java-nativec-loud/src/curso-java/renato/segundo arquivo java/semanaUm.java
@@ -0,0 +1,7 @@
+public class semanaUm{
+    public static void main(String[] args) {
+        System.err.println("Bem-vindo a meu curso de java native cloud");
+    }
+
+
+}
